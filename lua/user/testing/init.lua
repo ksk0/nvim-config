d130918cc632afbc76d56ac78d6a139f84d3cabc
koske <dkoscic@riteh.hr>
2022-12-21 18:29:55 +0100

    Fixed error in "testing/init" script

--- lua/user/testing/init.lua
+++ lua/user/testing/init.lua
@@ -1,18 +1,17 @@
 local api = vim.api
 local pt = require ("project-tools")
+local test_runner = function () print("There is no project file") end
+
 local project = pt.config.load()
 
-local test_runner
-local ok_tester,tester = pcall (require, "user.testing." .. project.lang)
+if project then
+  local ok_tester,tester = pcall (require, "user.testing." .. project.lang)
 
-if ok_tester then
-  test_runner = tester.setup(project)
-else
-  test_runner = function () print("There is no lang " .. project.lang) end
+  if ok_tester then
+    test_runner = tester.setup(project)
+  else
+    test_runner = function () print("There is no lang " .. project.lang) end
+  end
 end
 
 api.nvim_create_user_command("RunTests", test_runner, {})
-
-
-
-
