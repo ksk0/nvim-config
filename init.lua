21cb71e2353c03bc2a62718166364894e23ce26e
koske <dkoscic@riteh.hr>
2022-12-21 18:29:23 +0100

    Refactored "init.lua" script

--- init.lua
+++ init.lua
@@ -1,31 +1,53 @@
-require "user.initialize"
+local modules = {
+  "user.initialize",
 
-require "user.options"                -- done
-require "user.keymaps"                -- done
-require "user.completion"             -- done
+  "user.options",                -- done
+  "user.keymaps",                -- done
+  "user.completion",             -- done
 
--- require "user.illuminate"             -- done (maybe later)
-require "user.lsp"                    -- done
-require "user.dap"                    -- done
-require "user.telescope"              -- done
-require "user.treesitter"             -- done
-require "user.autopairs"              -- done
-require "user.comment"                -- done
-require "user.gitsigns"               -- done
-require "user.nvim-tree"              -- done
-require "user.bufferline"             -- done
-require "user.lualine"                -- done
-require "user.toggleterm"             -- done
-require "user.project"                -- not in use
-require "user.impatient"              -- done
-require "user.indentline"             -- done
-require "user.alpha"                  -- done
-require "user.whichkey"               -- done (left for later)
-require "user.autocommands"           -- done
-require "user.testing"
-require "user.runner".setup()         -- done
+  "user.lsp",                    -- done
+  "user.dap",                    -- done
+  "user.telescope",              -- done
+  "user.treesitter",             -- done
+  "user.autopairs",              -- done
+  "user.comment",                -- done
+  "user.gitsigns",               -- done
+  "user.nvim-tree",              -- done
+  "user.bufferline",             -- done
+  "user.lualine",                -- done
+  "user.toggleterm",             -- done
+  "user.project",                -- not in use
+  "user.impatient",              -- done
+  "user.indentline",             -- done
+  "user.alpha",                  -- done
+  "user.whichkey",               -- done (left for later)
+  "user.autocommands",           -- done
+  "user.testing",
+  "user.runner",                 -- done
+  "user.initialize",
 
--- require "user.neo-tree"
--- require "user.flexy-files"
+  "user.options",                -- done
+  "user.keymaps",                -- done
+  "user.completion",             -- done
+
+  -- "user.illuminate",          -- done (maybe later)
+  -- "user.neo-tree",
+  -- "user.flexy-files",
+}
+
+local timer = require("user.timer")
+local pass  = timer.pass()
+local total = timer.total()
+
+pass()
+total()
+
+
+for _,module in ipairs(modules) do
+  require(module)
+end
+
 
 vim.cmd [[command! -nargs=+ HH :vert help <args>]]
+
+total("Total load time")
