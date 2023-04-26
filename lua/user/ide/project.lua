0688b1c6d5b317e3b4466a701eaa74023b30baa3
koske <ksk0@users.noreplay.github.com>
2023-04-26 12:39:53 +0200

    Do nothing if "project-tools" are not installed

--- lua/user/ide/project.lua
+++ lua/user/ide/project.lua
@@ -1,5 +1,7 @@
-local project = require("project-tools"):load()
+local ok, ptools = pcall(require, "project-tools")
+if not ok then return  end
 
+local project = ptools:load()
 if not project then return  end
 
 project:setup()
