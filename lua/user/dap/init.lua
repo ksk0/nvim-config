2b8e6a116e532dcf47e77b6650af1d9b20567f45
koske <ksk0@users.noreplay.github.com>
2023-04-26 10:32:31 +0200

    Removed "keymaps" from "dap" configuration

--- lua/user/dap/init.lua
+++ lua/user/dap/init.lua
@@ -1,3 +1,11 @@
 require "user.dap.dap-ui"
 require "user.dap.adapters"
 require "user.dap.keymaps"
+
+
+-- ============================================================================
+-- Set icons for some debugign "points/stages"
+--
+vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', {text='', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='', texthl='', linehl='', numhl=''})
