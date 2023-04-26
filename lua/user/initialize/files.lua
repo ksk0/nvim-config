d6b615589048b69580dd5997c43b3368c99fc2cf
koske <ksk0@users.noreplay.github.com>
2023-04-26 09:13:01 +0200

    Deleted obsolete files in "initialize"

--- lua/user/initialize/files.lua
+++
@@ -1,9 +0,0 @@
-local config_root = vim.fn.stdpath('config') .. "/"
-
-return {
-  hashes       = config_root .. ".hashes",
-  plugins      = config_root .. "lua/user/plugins.lua",
-  packages     = config_root .. "lua/user/initialize/packages.lua",
-  lsp_servers  = config_root .. "lua/user/lsp/servers.lua",
-  ts_languages = config_root .. "lua/user/treesitter/languages.lua",
-}
